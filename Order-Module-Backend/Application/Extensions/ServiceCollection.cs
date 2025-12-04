5bd92c5213157a5c96607cf309db5df7b0ff96e0
qn12323 <quocninh9.32017.com>
2025-12-04 14:59:52 +0700

    feat(application): add ServiceCollection for dependency injection registration

---
+++ Order-Module-Backend/Application/Extensions/ServiceCollection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.Extensions
+{
+    public static class ServiceCollection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddAuthenticationServices(configuration);
+            services.RegisterServices(configuration);
+            return services;
+        }
+
+        public static IServiceCollection AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //
+            return services;
+        }
+
+        private static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //
+            return services;
+        }
+    }
+}
