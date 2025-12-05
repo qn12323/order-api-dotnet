83cccf9ccde969702e06f0fb2ebccffa8c22fdf9
qn12323 <quocninh9.32017.com>
2025-12-05 11:36:45 +0700

    feat(infrastructure): add dbcontext

---
+++ Order-Module-Backend/Infrastructure/Sql/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Sql
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
