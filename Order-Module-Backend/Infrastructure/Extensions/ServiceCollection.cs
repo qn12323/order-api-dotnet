2c820028c138306a875ce3352ad220b38b382c92
qn12323 <quocninh9.32017.com>
2025-12-05 12:55:52 +0700

    feat(infrastructure): register repositories and DbContext with dependency injection

---
+++ Order-Module-Backend/Infrastructure/Extensions/ServiceCollection.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Infrastructure.Sql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions
+{
+    public static class ServiceCollection
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("OrderSystemDB");
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+            services.RegisterServices();
+            return services;
+        }
+
+        private static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepo<,>), typeof(GenericRepo<,>));
+            services.AddScoped<INotificationRepo, NotificationRepo>();
+            services.AddScoped<IOrderCancelRepo, OrderCancelRepo>();
+            services.AddScoped<IOrderItemRepo, OrderItemRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderReturnRepo, OrderReturnRepo>();
+            services.AddScoped<IPaymentRepo, PaymentRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<IUserRepo, UserRepo>();
+
+            return services;
+        }
+    }
+}
