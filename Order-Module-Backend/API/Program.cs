d6484c7af7327b80a226d3e78a17c64261dd94d8
qn12323 <quocninh9.32017.com>
2025-12-06 14:09:47 +0700

    feat(API): add database connection string and register Application & Infrastructure services in Program.cs

--- Order-Module-Backend/API/Program.cs
+++ Order-Module-Backend/API/Program.cs
@@ -1,11 +1,20 @@
+using Application.Extensions;
+using Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo order module", Version = "v1" });
+});
 
 var app = builder.Build();
 
